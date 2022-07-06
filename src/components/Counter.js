94a3fc01ae2f3797532a2856cbce961e876c895c
moath143 <moath2sweet@hotmail.com>
2022-07-06 13:43:21 +0300

    fix delay for gifs and many requests of API

---
+++ src/components/Counter.js
@@ -0,0 +1,12 @@
+import React, {useState} from 'react'
+
+const Counter = () => {
+    const [counter, setCounter] = useState(3);
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Counter
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,21 @@
 import React, { useEffect, useState } from "react";
 import CRUDRequest from "../API/giphy";
-import './home.css'
+import "./home.css";
 
 const Home = () => {
-    const [gifs, setGifs] = useState([]);
-    const [search, setSearch] = useState('');
-    const [counter, setCounter] = useState(3);
-    const[hide, setHide] = useState(false);
+  const [gifs, setGifs] = useState([]);
+  const [search, setSearch] = useState("");
+  const [counter, setCounter] = useState(3);
+  // const [hide, setHide] = useState(false);
 
+  const handleChange = (e) => { 
+    setSearch(e.target.value);
+    setCounter(3);
+  }
 
   const fetchData = async () => {
-     await CRUDRequest.get(
-      `search?api_key=FzKk0yVso7D0EqYys6BAlYAWjmATcGVI&q=${counter === 0 ? (search || 'tom and jeary'): setHide(false)}&limit=10&offset=0`
+    await CRUDRequest.get(
+      `search?api_key=FzKk0yVso7D0EqYys6BAlYAWjmATcGVI&q=${search || "tom and jeary"}&limit=10&offset=0`
     )
       .then((res) => {
         setGifs(res.data.data);
@@ -19,51 +23,45 @@ const Home = () => {
       .catch((err) => {
         console.log(err);
       });
-    };
+  };
 
-
-
-    const dataList = gifs.map((item) => {
-        console.log(item);
-        return (
-          <div className="images" key={item.id}>
-            <img className="image" src={item.images.original.url} alt='giphy' />
-          </div>
-        );
-    })
-
-    useEffect(() => { 
-        setTimeout(() => {
-            if (counter === 0) {
-                setHide(true);
-                fetchData();
-            } else {
-                setCounter(counter - 1);
-            }
-         }, 1000);
-    });
-    
-
-
-
-
+  const dataList = gifs.map((item) => {
     return (
-      <section className="gallery">
-        <div className="container">
-          <h1>Search Giphy</h1>
-          <div className="search">
-            <input
-              placeholder="Search..."
-              type="text"
-              onChange={(e) => setSearch(e.target.value)}
-              value={search}
-            />
-            <div className={hide ? "hide" : ""}>{counter}</div>
-          </div>
-        </div>
-        <div className="container">{counter === 0 ? dataList : ''}</div>
-      </section>
+      <div className="images" key={item.id}>
+        <img className="image" src={item.images.original.url} alt="giphy" />
+      </div>
     );
+  });
+
+
+    setTimeout(() => {
+      if (counter === 0) {
+      } else {
+        setCounter(counter - 1);
+      }
+    }, 1000);
+
+  useEffect(() => {
+    fetchData();
+  }, [search]);
+
+  return (
+    <section className="gallery">
+      <div className="container">
+        <h1>Search Giphy</h1>
+        <div className="search">
+          <input
+            placeholder="Search..."
+            type="text"
+            onChange={handleChange}
+            value={search}
+          />
+          <div className={counter === 0 ? "hide" : ""}>{counter}</div>
+        </div>
+      </div>
+      <div className="container">{counter === 0 ? dataList : ''}</div>
+    </section>
+  );
 };
 
 export default Home;
